707a1ff3e7ad30b1283fcd254c9f89286d687aea
Arman Darmesh <darmesharman@gmail.com>
2020-10-26 16:33:55 +0600

    add tags to welcome page articles

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -1,34 +1,34 @@
 @extends('layout')
 
-@section('content')   
-    <h2>Статьи</h2>
-    @auth
-        <a href="/articles/create" class="btn btn-warning btn-lg pull">Добавить статью</a>
-    @endauth
-    <hr>
+@section('content')
+<h2>Статьи</h2>
+@auth
+    <a href="/articles/create" class="btn btn-warning btn-lg pull">Добавить статью</a>
+@endauth
+<hr>
 
-    @forelse ($articles as $article)
-        <div class="row">
-            <div class="well clearfix">
-                <div class="text-center">
-                    <h3>{{ $article->title }}</h3>
-                    <p>{{ $article->excerpt }}</p>
-                    @foreach ($article->tags as $tag)
-                        <a href="{{ route('articles.index', ['tag' => $tag->name]) }}" 
-                            class="btn btn-outline-primary btn-sm">
-                            #{{ $tag->name }}
-                        </a>                        
-                    @endforeach
-                </div>
-                <div class="col-lg-12">
-                    <a href="/articles/{{ $article->id }}" class="btn btn-lg btn-warning pull-right">Подробнее</a>
-                    
-                </div>
+@forelse($articles as $article)
+    <div class="row">
+        <div class="well clearfix">
+            <div class="text-center">
+                <h3>{{ $article->title }}</h3>
+                <p>{{ $article->excerpt }}</p>
+                @foreach($article->tags as $tag)
+                    <a href="{{ route('articles.index', ['tag' => $tag->name]) }}"
+                        class="btn btn-outline-primary btn-sm">
+                        #{{ $tag->name }}
+                    </a>
+                @endforeach
+            </div>
+            <div class="col-lg-12">
+                <a href="/articles/{{ $article->id }}" class="btn btn-lg btn-warning pull-right">Подробнее</a>
+
             </div>
         </div>
-    @empty
-        <div class="row text-center">
-            <p>Sorry there is no artilces with this tag yet :(</p>
-        </div>
-    @endforelse
+    </div>
+@empty
+    <div class="row text-center">
+        <p>Sorry there is no artilces with this tag yet :(</p>
+    </div>
+@endforelse
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,71 +1,83 @@
 @extends('layout')
 
 @section('content')
-  <!-- NewFilmsSection-->
-  <h2>
-    Новые фильмы <!-- Title of NewFilmsSection-->
-  </h2>
-  <hr />
-  <!-- New Films -->
-  <div class="row">
+<!-- NewFilmsSection-->
+<h2>
+    Новые фильмы
+    <!-- Title of NewFilmsSection-->
+</h2>
+<hr />
+<!-- New Films -->
+<div class="row">
     <div class="films_block col-lg-3 col-md-3 col-sm-3 col-xs-6">
-      <img src="img/max.png" alt="max" />
-      <div class="film_label">Безумный макс</div>
+        <img src="img/max.png" alt="max" />
+        <div class="film_label">Безумный макс</div>
     </div>
     <div class="films_block col-lg-3 col-md-3 col-sm-3 col-xs-6">
-      <img src="img/cloud.png" alt="max" />
-      <div class="film_label">Облычный атлас</div>
+        <img src="img/cloud.png" alt="max" />
+        <div class="film_label">Облычный атлас</div>
     </div>
     <div class="films_block col-lg-3 col-md-3 col-sm-3 col-xs-6">
-      <img src="img/inter.png" alt="max" />
-      <div class="film_label">Интерстеллар</div>
+        <img src="img/inter.png" alt="max" />
+        <div class="film_label">Интерстеллар</div>
     </div>
     <div class="films_block col-lg-3 col-md-3 col-sm-3 col-xs-6">
-      <img src="img/matrix.png" alt="matrix" />
-      <div class="film_label">Матрица</div>
+        <img src="img/matrix.png" alt="matrix" />
+        <div class="film_label">Матрица</div>
     </div>
-  </div>
-  <div class="margin-8"></div> <!-- Just empty space -->
-  <!-- endNewFilmsSection-->
+</div>
+<div class="margin-8"></div> <!-- Just empty space -->
+<!-- endNewFilmsSection-->
 
-  <!-- NewSeriesSection -->
-  <h2>
-    Новые сериалы <!-- Title of NewSeriesSection -->
-  </h2>
-  <hr />
-  <!-- New Series -->
-  <div class="row">
+<!-- NewSeriesSection -->
+<h2>
+    Новые сериалы
+    <!-- Title of NewSeriesSection -->
+</h2>
+<hr />
+<!-- New Series -->
+<div class="row">
     <div class="films_block col-lg-3 col-md-3 col-sm-3 col-xs-6">
-      <img src="img/breakingbad.png" alt="max" />
-      <div class="film_label">Во все тяжкие</div>
+        <img src="img/breakingbad.png" alt="max" />
+        <div class="film_label">Во все тяжкие</div>
     </div>
     <div class="films_block col-lg-3 col-md-3 col-sm-3 col-xs-6">
-      <img src="img/dead.png" alt="max" />
-      <div class="film_label">Ходячие мертыецы</div>
+        <img src="img/dead.png" alt="max" />
+        <div class="film_label">Ходячие мертыецы</div>
     </div>
     <div class="films_block col-lg-3 col-md-3 col-sm-3 col-xs-6">
-      <img src="img//silicon.png" alt="max" />
-      <div class="film_label">Силикон</div>
+        <img src="img//silicon.png" alt="max" />
+        <div class="film_label">Силикон</div>
     </div>
     <div class="films_block col-lg-3 col-md-3 col-sm-3 col-xs-6">
-      <img src="img/xfiles.png" alt="max" />
-      <div class="film_label">Люди икс</div>
+        <img src="img/xfiles.png" alt="max" />
+        <div class="film_label">Люди икс</div>
     </div>
-  </div>
-  <div class="margin-8"></div> <!-- Just an empty space -->
-  <!-- endNewSeriesSection-->
+</div>
+<div class="margin-8"></div> <!-- Just an empty space -->
+<!-- endNewSeriesSection-->
 
-  <section>
-    @foreach ($articles as $article)
-      <article>
-        <a href="#"><h3>{{ $article->title }}</h3></a>
-        <hr />
-        <p>
-          {{ $article->excerpt }}
-        </p>
-        <a href="articles/{{ $article->id }}" class="btn btn-warning pull-right">Читать</a>
-        <div class="clear"></div>
-      </article>
+<section>
+    @foreach($articles as $article)
+        <article>
+            <a href="#">
+                <h3>{{ $article->title }}</h3>
+            </a>
+            <hr />
+            <p>
+                {{ $article->excerpt }}
+            </p>
+            <p>
+                @foreach($article->tags as $tag)
+                    <a href="{{ route('articles.index', ['tag' => $tag->name]) }}"
+                        class="btn btn-outline-primary btn-sm">
+                        #{{ $tag->name }}
+                    </a>
+                @endforeach
+            </p>
+            <a href="articles/{{ $article->id }}" class="btn btn-warning pull-right">Читать</a>
+            <div class="clear"></div>
+        </article>
     @endforeach
-  </section>
+</section>
 @endsection
